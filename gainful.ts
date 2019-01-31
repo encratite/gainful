a4425fe5f2d5bee52cf51883d5f8d7ea594fa264
void <git.email.address@googlemail.com>
2019-02-01 00:03:45 +0100

    Almost no progress.

--- gainful.ts
+++ gainful.ts
@@ -18,17 +18,17 @@ export class Player {
 	async render(container: HTMLElement) {
 		this.container = container;
 		this.container.classList.add("gainfulPlayer");
-		const createDiv = () => this.createElement<HTMLDivElement>("div", this.container);
+		const createDiv = () => Dom.createElement<HTMLDivElement>("div", this.container);
 		this.topRow = createDiv();
 		this.bottomRow = createDiv();
-		this.trackSelect = this.createElement<HTMLSelectElement>("select", this.topRow, {
+		this.trackSelect = Dom.createElement<HTMLSelectElement>("select", this.topRow, {
 			disabled: true,
 		});
-		this.playButton = this.createButton("Play", () => this.onPlayButtonClick());
-		this.stopButton = this.createButton("Stop", () => this.onStopButtonClick());
+		this.playButton = Dom.createButton("Play", this.bottomRow, () => this.onPlayButtonClick());
+		this.stopButton = Dom.createButton("Stop", this.bottomRow, () => this.onStopButtonClick());
 		await this.samplePromise;
 		this.samples.forEach((sample) => {
-			const option = this.createElement<HTMLOptionElement>("option", this.trackSelect, {
+			const option = Dom.createElement<HTMLOptionElement>("option", this.trackSelect, {
 				value: sample.path,
 				textContent: sample.name,
 			});
@@ -43,25 +43,6 @@ export class Player {
 			const decodePromises = this.samples.map((sample) => sample.decode(this.audioContext));
 			await Promise.all(decodePromises);
 		}
-	}
-	
-	createElement<T extends HTMLElement>(tagName: string, parent: HTMLElement, properties: Object = {}): T {
-		const element = <T>document.createElement(tagName);
-		parent.appendChild(element);
-		for  (let property in properties) {
-			const value = properties[property];
-			element[property] = value;
-		}
-		return element;
-	}
-
-	createButton(label: string, handler: () => void): HTMLButtonElement {
-		const button = this.createElement<HTMLButtonElement>("button", this.bottomRow, {
-			textContent: label,
-			disabled: true,
-			onclick: (e: MouseEvent) => handler(),
-		});
-		return button;
 	}
 	
 	async onPlayButtonClick() {
@@ -124,5 +105,76 @@ export class Sample {
 			this.sourceNode.stop();
 			delete this.sourceNode;
 		}
+	}
+}
+
+export abstract class Effect {
+	parameters: EffectParameter[];
+
+	abstract process(input: Float32Array[]): Float32Array[];
+
+	render(container: HTMLElement) {
+		const table = Dom.createElement<HTMLTableElement>("table", container);
+		for (let parameter of this.parameters) {
+			const row = Dom.createElement<HTMLTableRowElement>("tr", table);
+			const nameCell = Dom.createElement<HTMLTableCellElement>("td", row, {
+				textContent: parameter.name
+			});
+			const parameterCell = Dom.createElement<HTMLTableCellElement>("td", row);
+			parameter.render(parameterCell);
+		};
+	}
+}
+
+export class EffectParameter {
+	name: string;
+	defaultValue: number;
+	min: number;
+	max: number;
+	step: number;
+
+	value: number;
+
+	constructor(name: string, defaultValue: number, min: number, max: number, step: number) {
+		this.name = name;
+		this.defaultValue = defaultValue;
+		this.min = min;
+		this.max = max;
+		this.step = step;
+		this.value = defaultValue;
+	}
+
+	render(container: HTMLElement) {
+		const range = Dom.createElement<HTMLInputElement>("input", container, {
+			type: "range",
+			min: this.min,
+			max: this.max,
+			step: this.step,
+			value: this.value
+		});
+		const input = Dom.createElement<HTMLInputElement>("input", container, {
+			value: this.value
+		});
+	}
+}
+
+class Dom {
+	static createElement<T extends HTMLElement>(tagName: string, parent: HTMLElement, properties: Object = {}): T {
+		const element = <T>document.createElement(tagName);
+		parent.appendChild(element);
+		for  (let property in properties) {
+			const value = properties[property];
+			element[property] = value;
+		}
+		return element;
+	}
+
+	static createButton(label: string, container: HTMLElement, handler: () => void): HTMLButtonElement {
+		const button = Dom.createElement<HTMLButtonElement>("button", container, {
+			textContent: label,
+			disabled: true,
+			onclick: (e: MouseEvent) => handler(),
+		});
+		return button;
 	}
 }
